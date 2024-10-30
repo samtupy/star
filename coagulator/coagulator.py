363c007c139e7324463e24a67165381ece51aedb
Sam Tupy <webmaster@samtupy.com>
2024-10-30 05:01:25 -0500

    initial commit

---
+++ coagulator/coagulator.py
@@ -0,0 +1,101 @@
+import json
+import random
+import re
+import time
+from websocket_server import WebsocketServer
+
+def g(): pass #globals
+g.provider_rev = 1
+g.user_rev = 1
+
+def parse_speech_meta(meta):
+	"""Takes speech metadata such as "Sam" or "Sam<r=4,p=-2>" and returns a dictionary of parsed properties such as voice, rate and pitch."""
+	if meta.find("<") < 0: return {"voice": meta}
+	voice, part, params = meta.partition("<")
+	result = {"voice": voice}
+	params = params[:-1]
+	params = params.split(",")
+	for p in params:
+		try:
+			p = p.strip().split("=")
+			if len("p") < 2: continue
+			if p[0] == "r": result["rate"] = float(p[1])
+			elif p[0] == "p": result["pitch"] = float(p[1])
+		except ValueError: continue
+
+def find_provider_for_voice(voice):
+	"""Searches the list of voices for a provider to send a speech request to given a voice name."""
+	for v in g.voices:
+		if re.search(r"\b" + voice + r"\b", v): return (v, random.choice(g.voices[v]))
+	return (voice, None)
+
+def handle_speech_request(client, server, request):
+	"""Receives a list of requests such as ["Sam: hello", "Alex<p=4>: What's up!"] and dispatches each line to a voice provider for synthesis."""
+	if not "speech_sequence" in client: client["speech_sequence"] = 0
+	if type(request) == str: request = [request]
+	for line in request:
+		raw_meta, part, text = line.partition(": ")
+		if not text.strip():
+			server.send_message(client, json.dumps({"warning": "no text found in line {line}"}))
+			continue
+		meta = parse_speech_meta(raw_meta)
+		if not "voice" in meta:
+			server.send_message(client, json.dumps({"warning": f"failed to parse voice meta {raw_meta}"}))
+			continue
+		meta["voice"], provider = find_provider_for_voice(meta["voice"])
+		if not provider:
+			server.send_message(client, json.dumps({"warning": f"failed to find provider for {meta['voice']}"}))
+			continue
+		client["speech_sequence"] += 1
+		meta["text"] = text
+		meta["id"] = f"{client['id']}_{client['speech_sequence']}"
+		server.send_message(provider, json.dumps(meta))
+		g.speech_requests[meta["id"]] = client
+
+def on_message(client, server, message):
+	"""This callback is fired every time we receive a new message on our server websocket. We handle both provider and user messages here."""
+	msg = {}
+	try:
+		msg = json.loads(message)
+	except: return
+	if "provider" in msg and "voices" in msg:
+		if msg["provider"] < g.provider_rev:
+			server.send_message(client, json.dumps({"error", f"must be revision {g.provider_rev} or higher"}))
+			return
+		for v in msg["voices"]:
+			if v in g.voices: g.voices[v].append(client)
+			else: g.voices[v] = [client]
+	elif "user" in msg:
+		if msg["user"] < g.user_rev:
+			server.send_message(client, json.dumps({"error", f"must be revision {g.user_rev} or higher"}))
+			return
+		if "request" in msg: handle_speech_request(client, server, msg["request"])
+		else: server.send_message(client, json.dumps({"voices": list(g.voices)}))
+	elif "speech" in msg and msg["speech"] in g.speech_requests and "data" in msg:
+		server.send_message(g.speech_requests[msg["speech"]], message)
+		del(g.speech_requests[msg["speech"]])
+
+def on_lost_client(client, server):
+	"""If a provider disconnects, we must remove it's list of voices. If a user disconnects mid-synthesis, we must remove it's speech requests."""
+	for v in list(g.voices):
+		if client in g.voices[v]:
+			g.voices[v].remove(client)
+			if len(g.voices[v]) == 0: del(g.voices[v])
+	for r in list(g.speech_requests):
+		if g.speech_requests[r] == client: del(g.speech_requests[r])
+
+def main():
+	g.speech_requests = {}
+	g.voices = {}
+	g.ws = WebsocketServer(port = 7774, host = "0.0.0.0")
+	g.ws.set_fn_client_left(on_lost_client)
+	g.ws.set_fn_message_received(on_message)
+	g.ws.run_forever()
+	try:
+		while 1: time.sleep(5)
+	except KeyboardInterrupt:
+		print("shutting down")
+		g.ws.shutdown_gracefully()
+
+if __name__ == "__main__":
+	main()
---
+++ readme.md
@@ -0,0 +1,42 @@
+# Speech To Audio Relay (STAR)
+This is a set of components intended to ease the creation of audio productions that involve the synthesis of text to speech to audio, particularly where many voices that might be contained on any number of different computers or devices are involved.
+
+This setup involves 3 components:
+
+## User client
+The frontend to this application involves the user client. This application is responsible for requesting synthesis and outputting the results.
+
+## Speech Providers
+The task of these components is to translate text to speech depending on the voices available on a given system. A provider can run anywhere TTS voices are available, and some providers might be platform-agnostic as they might synthesize tts from cloud providers like Google or Amazon.
+
+## Coagulation server
+This component is what ties everything together. A coagulator is run, and then speech providers connect to it and send a list of voices it can synthesize. The coagulator will take note of voice names to provider clients. When a user client connects to the coagulator, it can therefor send a long speech script to the coagulator which is then parsed, whereupon requests per voice are sent on to the various speech providers that can synthesize each voices. The coagulator then acts as a relay, sending the audio data from each provider back to the user client that initially asked for it.
+
+## API
+
+This project uses web sockets for communication. The coagulator is what acts as the web socket server, and is written in python. Both speech providers and user clients connect to it. All payloads are in json, and I can't imagine a way to make the API any more simple.
+
+### Writing a user client
+For a user client to communicate with the coagulator, it should send a payload in the form:
+```{"user": revision, "request": ["voice1: line1", "voice2: line2", "someone<r=-5>": "etc"]}```
+If the text key is omited, the coagulator will return a list of full voice names available in the form `{"voices": ["voice1", "voice2"]}`
+
+Otherwise, the coagulator will start sending back payloads in the form:
+```{"speech": ID, "data": BASE64_ENCODED_WAV}```
+Until all lines passed have been synthesized.
+
+While the coagulator does act as a direct relay and thus providers could send data in any format it wants and could even include extra keys in the response, what's written here is the standard.
+
+The speech key is important. It contains an ID used to track the order of synthesis. It is made up of 2 numbers separated by an underscore. The first number usually isn't importaant, it's the integer client ID that you are known as by the coagulator. The second number however contains an integer that denotes the fragment number that this speech payload is referencing. When outputting data, you should number or sequence your output based on this second number.
+
+### Writing a speech provider
+When a speech provider first connects to a coagulator, it should send a packet in the form:
+```{"provider": revision, "voices": ["voice1", "voice2", "etc"]}```
+before continuously listening for packets in the form:
+```{"voice": "voicename", "text": "text to speak", "id": ID}```
+and responding each time with packets such as:
+```{"speech": ID, "data": BASE64_ENCODED_WAV}```
+where ID is the same as that received in the voice packet's id key.
+
+Voice packets might contain extra parameters like rate and pitch, but your providers should be set up to not require these.
+
